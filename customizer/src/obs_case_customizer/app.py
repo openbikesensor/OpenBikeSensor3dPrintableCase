275cf2bf569874ae0feef64e8f3228e2ba104bda
gluap <github@pgoergen.de>
2022-01-26 21:52:11 +0100

    pass defines properly

--- customizer/src/obs_case_customizer/app.py
+++ customizer/src/obs_case_customizer/app.py
@@ -1,8 +1,8 @@
 import asyncio
 import inspect
+import json
 import logging
 import os
-import json
 import shutil
 import tempfile
 import typing
@@ -10,12 +10,11 @@ import uuid
 from pathlib import Path
 
 import pkg_resources
-from fastapi import FastAPI, Form, File, UploadFile, Request, Depends
+from fastapi import FastAPI, Form, File, Request, Depends
+from fastapi.responses import HTMLResponse
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.websockets import WebSocket
-from fastapi.responses import HTMLResponse
-
 from pydantic import BaseModel
 
 queue = asyncio.Queue(maxsize=20)
@@ -33,37 +32,52 @@ def models(root):
 MODEL_ROOT = Path(os.path.dirname(__file__) + "/../../../src").resolve()
 
 
-async def convert_scad_file(scad_file: str, dir_to_work: Path):
+def scad_arguments_from_json(json_file: Path):
+    """
+    makes a list of "-D <variable> <value>" for openscad as customizer json does not work with includes
+    :param json_file:
+    :return:
+    """
+    data = json.load(json_file.open())
+    cv = CustomVariables()
+    scad_defines = []
+    for name, value in data.items():
+        if cv.dict()[name] != value:
+            scad_defines.extend(["-D", f"{name}={str(value)}"])
+    return scad_defines
+
+
+async def convert_scad_file(scad_file: str, target_basedir: Path):
+    """
+    Convert a .scad file to .stl mirroring the scad directory structure
+    :param scad_file:
+    :param target_basedir:
+    :return:
+    """
     logging.error(f"converting {scad_file}")
-    logging.info(f"converting {scad_file} in {dir_to_work}")
-    variables_json_file = dir_to_work / "variables.json"
+    logging.info(f"converting {scad_file} in {target_basedir}")
+    variables_json_file = target_basedir / "variables.json"
 
     modelfile = MODEL_ROOT / scad_file
-    targetfile = dir_to_work / str(scad_file).replace("scad", "stl")
+    targetfile = target_basedir / str(scad_file).replace("scad", "stl")
     targetfile.parent.mkdir(parents=True, exist_ok=True)
+
     assert targetfile.parent.is_dir()
     assert variables_json_file.is_file()
     assert modelfile.is_file()
-    p = await asyncio.create_subprocess_exec("openscad",
-                                       str(modelfile),
-                                       "-p", str(variables_json_file), "-o",
-                                       str(targetfile))
+
+    scad_defines = scad_arguments_from_json(variables_json_file)
+
+    p = await asyncio.create_subprocess_exec("openscad", str(modelfile), *scad_defines, "-o", str(targetfile))
+
     try:
         await asyncio.wait_for(p.wait(), 200)
-        return True
-    except asyncio.TimeoutError:
-        logging.error(f"timeout for {scad_file}")
+        if p.returncode is not None and p.returncode == 0:
+            return True
         return False
 
-
-async def run_make():
-    p = await asyncio.create_subprocess_exec("make",
-                                             "-j", cwd=Path(os.path.dirname(__file__)).parent.parent.parent)
-    try:
-        await asyncio.wait_for(p.wait(), 200)
-        return True
     except asyncio.TimeoutError:
-        logging.error(f"timeout for")
+        logging.error(f"timeout for {scad_file}")
         return False
 
 
@@ -77,11 +91,14 @@ async def run_job(dir_to_work):
         logging.error(f" run_job got {dir_to_work} go")
         shutil.copy(dir_to_work / "variables.json", temp / "variables.json")
         logging.error(f" run_job got {dir_to_work}")
-
+        project_success = True
         for scad_file in list(models(MODEL_ROOT)):
             logfile.open("a").write(f"converting {scad_file}")
             success = await convert_scad_file(scad_file.relative_to(MODEL_ROOT), temp)
-            logfile.open("a").write(f"{'successful' if success else 'failed'}\n")
+            logfile.open("a").write(f" {'successful' if success else 'failed'}\n")
+            project_success = project_success and success
+
+        logfile.open("a").write(f"conversion completed {'with some errors' if not project_success else ''}\n")
 
 
 async def worker():
@@ -94,10 +111,9 @@ async def worker():
 
 @app.on_event("startup")
 async def start_worker():
-    logging.error("statup")
     app.convert_workers = []
     for i in range(3):
-        logging.error(f"starting worker {i}")
+        logging.info(f"starting worker {i}")
         app.convert_workers.append(asyncio.create_task(worker()))
         logging.info(f"started worker {i}")
 
@@ -153,33 +169,23 @@ class CustomVariables(BaseModel):
     default_clearance: float = 0
 
 
-@app.post("/files/")
-async def create_file(
-        file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
-):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "fileb_content_type": fileb.content_type,
-    }
-
-
 @app.get("/")
 def form_get(request: Request):
     variables = CustomVariables()
     return templates.TemplateResponse('customizer.html', context={'request': request, 'fields': variables.dict()})
 
 
-@app.get("/job/{uid}",response_class=HTMLResponse)
+@app.get("/job/{uid}", response_class=HTMLResponse)
 async def job(request: Request, uid: uuid.UUID):
     return templates.TemplateResponse('job.html', context={'request': request, 'uuid': uid})
+
 
 @app.websocket("/jobstate/{uid}")
 async def jobstate(websocket: WebSocket, uid: uuid.UUID):
     await websocket.accept()
     logfile = Path(tempfile.gettempdir()) / str(uid) / "log.txt"
     while True:
-        l = logfile.open("r").read().replace("\n","<br/>")
+        l = logfile.open("r").read().replace("\n", "<br/>")
         await websocket.send_json(l)
         await asyncio.sleep(5)
 
